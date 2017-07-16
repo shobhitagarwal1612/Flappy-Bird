3628d664a8d6d1c91b8f533a947217ec7b56d8d6
Shobhit Agarwal <shobhitagarwal1612@gmail.com>
2017-07-17 01:32:58 +0530

    adds all elements to resource.js

--- src/resource.js
+++ src/resource.js
@@ -1,12 +1,18 @@
 var res = {
     background_png: "res/main_background.png",
-    game_name_png: "res/game_name.png",
     bird1_png: "res/bird1.png",
     bird2_png: "res/bird2.png",
     bird3_png: "res/bird3.png",
+    game_name_png: "res/game_name.png",
     game_over_png: "res/game_over.png",
+    get_ready_png: "res/get_ready.png",
+    medal_gold_png: "res/medal_gold.png",
+    medal_silver: "res/medal_silver.png",
+    pipe_png: "res/pipe.png",
     play_button_png: "res/play_button.png",
+    result_board_png: "res/result_board.png",
     road_base_png: "res/road_base.png",
+    tap_tap_png: "res/tap_tap.png",
 };
 
 var g_resources = [];
